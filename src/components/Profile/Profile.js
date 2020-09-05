7d78712ec1cf00503c84c6158ab9c3162b970003
lifeisegg <58962402+lifeisegg123@users.noreply.github.com>
2020-09-06 00:50:16 +0900

    little responsive & infos

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,14 +4,20 @@ import { useSelector, useDispatch } from "react-redux";
 import { makeStyles } from "@material-ui/core/styles";
 import { Avatar } from "@material-ui/core";
 
-const useStyles = makeStyles({
-  infoBar: {
-    margin: "10vh auto",
-    width: "80vw",
-    height: "300px",
-    display: "flex",
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "935px",
+    margin: "10vh auto 0 auto",
+    "@media (max-width:990px)": {
+      width: "99.4vw",
+    },
   },
-  infoSection: {},
+  infoBar: {
+    paddingBottom: "45px",
+    height: "150px",
+    display: "flex",
+    borderBottom: "solid 1px #dbdbdb",
+  },
   profileImgDiv: {
     padding: "0px 60px",
     marginRight: "10px",
@@ -20,18 +26,36 @@ const useStyles = makeStyles({
     width: "150px",
     height: "150px",
   },
+  infoSection: {
+    width: "100%",
+  },
+  infoLists: {
+    width: "50%",
+    display: "flex",
+    listStyle: "none",
+    paddingLeft: "0px",
+  },
+  infoListItem: {
+    paddingRight: "40px",
+  },
   timelinesImgDiv: {
-    width: "614px",
+    margin: "30px auto",
     display: "grid",
-    gridTemplateColumns: "1fr 1fr 1fr",
-    gridTemplateRows: "1fr 1fr 1fr",
-    placeItems: "center center",
+    gridTemplateColumns: "repeat(3, 1fr)",
+    placeItems: "center space-between",
   },
   timelinesImg: {
-    width: "25vw",
-    height: "25vw",
+    width: "293px",
+    height: "293px",
+    "&:hover": {
+      filter: "blur(3px)",
+    },
+    "@media (max-width:990px)": {
+      width: "29.8vw",
+      height: "29.8vw",
+    },
   },
-});
+}));
 
 function Profile() {
   const classes = useStyles();
@@ -44,33 +68,41 @@ function Profile() {
   return (
     <div>
       <NavBar userInfo={userInfo}></NavBar>
-      <div className={classes.infoBar}>
-        <div className={classes.profileImgDiv}>
-          <Avatar
-            src={userInfo.profileImage}
-            alt={userInfo.nickname}
-            className={classes.profileImg}
-          />
-        </div>
-        <section>
-          <div>
-            <h3>{userInfo.nickname}</h3>
+      <div className={classes.root}>
+        <div className={classes.infoBar}>
+          <div className={classes.profileImgDiv}>
+            <Avatar
+              src={userInfo.profileImage}
+              alt={userInfo.nickname}
+              className={classes.profileImg}
+            />
           </div>
-          <span>
-            게시물
-            <span>5</span>
-          </span>
-        </section>
-      </div>
-      <div className={classes.timelinesImgDiv}>
-        {timelines.map((timeline) => (
-          <img
-            key={`${timeline.timelineKey}`}
-            src={timeline.imgs[0]}
-            alt={timeline.desc}
-            className={classes.timelinesImg}
-          ></img>
-        ))}
+          <section className={classes.infoSection}>
+            <div>
+              <h3>{userInfo.nickname}</h3>
+            </div>
+            <ul className={classes.infoLists}>
+              <li className={classes.infoListItem}>
+                게시물
+                <span>{timelines.length}</span>
+              </li>
+              <li className={classes.infoListItem}>
+                팔로워
+                <span>{userInfo.friends && userInfo.friends.length}</span>
+              </li>
+            </ul>
+          </section>
+        </div>
+        <div className={classes.timelinesImgDiv}>
+          {timelines.map((timeline) => (
+            <img
+              key={`${timeline.timelineKey}`}
+              src={timeline.imgs[0]}
+              alt={timeline.desc}
+              className={classes.timelinesImg}
+            ></img>
+          ))}
+        </div>
       </div>
     </div>
   );
